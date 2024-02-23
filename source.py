c099e426d6ba23cad9467bee65ca6b389febdc79
Kota Tharun Reddy <tharunreddykota2001@gmail.com>
2024-02-23 15:00:43 +0530

    added source.py file

---
+++ source.py
@@ -0,0 +1,11 @@
+def get_source():
+    with open('source.txt', 'r') as file:
+        source=file.readlines()
+        source=[s.strip() for s in source]
+        return source
+
+def add_source():
+    source=input("Enter the source : ")
+    with open('source.txt', 'a') as file:
+        file.write('\n')
+        file.write(source)
