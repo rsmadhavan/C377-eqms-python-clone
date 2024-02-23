efeca59e1c1734e9118b13260c6e93bb28a7fcb2
rsmadhavan <rsmadhavan2000@gmail.com>
2024-02-23 15:34:14 +0530

    integrate source_menu with main

--- source.py
+++ source.py
@@ -9,26 +9,29 @@ def display_source():
         source=file.readlines()
         source=[s.strip() for s in source]
         for i in range(len(source)):
-            print(f"{i+1} : {source[i]}")
+            print(f"\t{i+1} : {source[i]}")
 
 def add_source():
-    source=input("ENTER THE SOURCE : ")
+    source=input("\n\tENTER THE SOURCE : ")
     with open('source.txt', 'a') as file:
         file.write('\n')
         file.write(source)
+    print("!!!! SOURCE ADDED SUCCESSFULLY !!!!\n")
 
 def source_menu():
-    print(f'''
-        SOURCE MENU
-        -------------
-        1. ADD SOURCE
-        2. DISPLAY SOURCE
-        3. GO BACK
-              ''')
-    choice=input("ENTER CHOICE : ")
-    if choice=='1':
-        add_source()
-    elif choice=='2':
-        display_source()
-    else:
-        print("... GOING BACK ...")
+    while True:
+        print(f'''
+            SOURCE MENU
+            -------------
+            1. ADD SOURCE
+            2. DISPLAY SOURCE
+            3. GO BACK
+                ''')
+        choice=input("\tENTER CHOICE : ")
+        if choice=='1':
+            add_source()
+        elif choice=='2':
+            display_source()
+        else:
+            print("\n... GOING BACK ...")
+            break
