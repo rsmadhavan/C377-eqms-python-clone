beb92fff0acfd30cc459e8e53c12031e598c4ef3
rsmadhavan <rsmadhavan2000@gmail.com>
2024-02-23 15:20:03 +0530

    add method to display list of sources

--- source.py
+++ source.py
@@ -4,6 +4,13 @@ def get_source():
         source=[s.strip() for s in source]
         return source
 
+def display_source():
+    with open('source.txt', 'r') as file:
+        source=file.readlines()
+        source=[s.strip() for s in source]
+        for i in range(len(source)):
+            print(f"{i+1} : {source[i]}")
+
 def add_source():
     source=input("Enter the source : ")
     with open('source.txt', 'a') as file:
